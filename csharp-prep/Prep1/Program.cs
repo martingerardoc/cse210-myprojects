527025fe9eef897102d5889f3332321109eae97d
martingerardoc <martingerardoc56@gmail.com>
2024-04-27 07:01:30 -0300

    prep 1 finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+         // ask the user his name
+        Console.Write("Hello What is your first name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last=Console.ReadLine();
+        Console.WriteLine($"your name is {last},{name} {last}");
+        Console.WriteLine("Thanks");
     }
 }

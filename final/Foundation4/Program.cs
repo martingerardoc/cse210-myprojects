4f148d2f6fe92c0e4187cdccbbcbda9f91ba03f9
martingerardoc <martingerardoc56@gmail.com>
2024-06-05 07:51:04 -0300

    Main program tested class swimming dont work

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,37 @@
 using System;
 
-class Program
+List<Exercise> exerciseList = new List<Exercise>();
+
+Running running = new Running("April 15, 2024", 30, 4.8);
+exerciseList.Add(running);
+
+StationaryBicycles stationaryBicycles = new StationaryBicycles("April 17, 2024", 60, 15);
+exerciseList.Add(stationaryBicycles);
+
+Swimming swimming = new Swimming("April 24, 2024", 10, 15);
+//exerciseList.Add(swimming);
+
+Console.WriteLine("Exercises:");
+Console.WriteLine();
+
+foreach (Exercise exercise in exerciseList)
+
 {
-    static void Main(string[] args)
+    Console.WriteLine(exercise.GetSummary());
+
+}
+
+internal class Swimming
+{
+    private string v1;
+    private int v2;
+    private int v3;
+
+    public Swimming(string v1, int v2, int v3)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
     }
+
 }
